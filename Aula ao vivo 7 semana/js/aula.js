7d6f521f30de164d64c0aba32872b85ff28fe9d2
Valmir-Gomes <92238636+Valmir-Gomes@users.noreply.github.com>
2022-06-17 22:48:31 -0300

    atualizacao

--- Aula ao vivo 7 semana/js/aula.js
+++ Aula ao vivo 7 semana/js/aula.js
@@ -1,4 +1,4 @@
-const mario = document.querySelector(",super-mario");
+const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
 
 const jump = () => {
@@ -10,3 +10,26 @@ const jump = () => {
 }
 
 document.addEventListener("keydown", jump);
+document.addEventListener("clixk", jump);
+document.addEventListener("touch", jump);
+
+const loopGame = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px", "");
+    if (pipePosition <= 120 && marioPosition > 0 && marioPosition < 80) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = "nome";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.scr = "../img/mario-game-over.png"
+        mario.style.width = "75px";
+        mario.style.marginLeft = "45px";
+
+        clearInterval(loopGame)
+    }
+}, 10);
+
